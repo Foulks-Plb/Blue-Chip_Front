a32272c8a068bb4e4a44a9d9f1edeb96178b031e
foulk-plb <bananox88@gmail.Com>
2022-12-10 19:18:09 -1200

    NEW: mainnet

--- src/variables/project.ts
+++ src/variables/project.ts
@@ -1,7 +1,7 @@
 export const addressContract = "0x9D78488f7112A8aC3eb0cC77d839105A70909902";
 
-export const RPC = "https://data-seed-prebsc-1-s3.binance.org:8545";
+export const RPC = "https://bsc-dataseed.binance.org/";
 
-export const chainId = 97;
+export const chainId = 56;
 
 export const numberOfMatch = 3;
